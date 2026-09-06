abbf013bf30166f96d1012613923b120c2c8e7b0
Manak05 <manakxkanam@gmail.com>
2026-09-06 12:36:59 +0530

    fix mysql port for railway

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
+  port: process.env.DB_PORT || 3306,
   database: process.env.DB_NAME || 'programwise',
   waitForConnections: true,
   connectionLimit: 10,
